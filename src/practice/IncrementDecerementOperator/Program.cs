715949540131d4a9bcc53acec563962010c433b8
Shamim448 <you@example.com>
2021-11-29 19:54:05 +0600

    Post Increment/Decrement

---
+++ src/practice/IncrementDecerementOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IncrementDecerementOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Post Increment/Decrement operator
+            Console.WriteLine("****Example of Post Increment****");
+            int i = 3;
+            Console.WriteLine(i);
+            Console.WriteLine(i++);
+            Console.WriteLine(i);
+
+           
+            Console.WriteLine("****Example of Post Decrement****");
+            int i2 = 3;
+            Console.WriteLine(i2);
+            Console.WriteLine(i2--);
+            Console.WriteLine(i2);
+        }
+    }
+}
