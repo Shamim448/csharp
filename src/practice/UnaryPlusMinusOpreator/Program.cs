69b5c819d4af43d4bda490612b2f40e28aca89f2
Shamim448 <you@example.com>
2021-11-29 20:11:54 +0600

    Unary Plus Minus Opreator

---
+++ src/practice/UnaryPlusMinusOpreator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UnaryPlusMinusOpreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(+4);
+            Console.WriteLine(-4);
+            Console.WriteLine(-(-4));
+        }
+    }
+}
