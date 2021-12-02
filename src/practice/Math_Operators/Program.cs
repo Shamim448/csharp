c45f353abd89bc5a44d9fc3b7f546fd4aaf65aff
Shamim448 <shamim.448@gmail.com>
2021-12-02 08:23:30 +0600

    Math operator

---
+++ src/practice/Math_Operators/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(16.8/4.1);        //By default it show double data type
+Console.WriteLine(16.8f / 4.1f);    //Floting point
+Console.WriteLine(16.8d / 4.1d);    //double type
+Console.WriteLine(16.8m / 4.1m);    //decimale type
+
