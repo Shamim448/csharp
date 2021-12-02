b9e523cab1115f703057904b7c5546f14fc48460
Shamim448 <shamim.448@gmail.com>
2021-12-02 09:43:23 +0600

    Logical operator

---
+++ src/practice/Logical_Operaor/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Example of Logical Operator!");
+uint a = 0b_1100_0011;
+uint b = 0b_1001_0011;
+uint c = a & b; // and operation 
+uint d = a | b; //Or operation
+uint e = a ^ b; //Xor Operation
+
+//And operation Both bit 1 hole result 1
+Console.WriteLine(Convert.ToString(c, toBase: 2));
+//Or operation jkono akta bit 1 hole result 1
+Console.WriteLine(Convert.ToString(d, toBase: 2));
+//XOR operation Both bit same hole result 0
+Console.WriteLine(Convert.ToString(e, toBase: 2));
+
+
+
+
+
