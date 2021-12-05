3175f9bfdaf5494fd76fc4535ff82840ca7a8329
Shamim448 <shamim.448@gmail.com>
2021-12-05 10:55:22 +0600

    String Method Compare();

---
+++ src/practice/Compare_Method/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Compare_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text1 = "shamim";
+            string text2 = "Shamim";
+            Console.WriteLine(String.Compare(text1, text2));
+        }
+    }
+}
