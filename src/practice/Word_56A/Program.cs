4b8da300e8419677c417be23393502ece3a753a7
Shamim448 <shamim.448@gmail.com>
2021-12-07 14:49:28 +0600

    Word_56A: check alphabet letter lowercase or uppercase

---
+++ src/practice/Word_56A/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Word_56A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int isLower = 0;
+            int isUpper = 0;
+            for(int i=0; i<str.Length; i++)
+            {
+                if (str[i] >= 'A' && str[i] <= 'Z') //65-91
+                {
+                    isUpper++;
+                }
+                else if(str[i] >= 'a' && str[i] <= 'z') //97-122
+                {
+                    isLower++;
+                }
+            }
+            if (isUpper > isLower)
+            {
+                Console.WriteLine(str.ToUpper());
+            }
+            else
+            {
+                Console.WriteLine(str.ToLower());
+            }
+        }
+    }
+}
