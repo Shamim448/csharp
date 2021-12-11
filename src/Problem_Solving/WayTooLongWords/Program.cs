0b8078cdb7ebf9bac734ad4ded2665cab1c69ab4
Shamim448 <shamim.448@gmail.com>
2021-12-11 14:42:32 +0600

    WayTooLongWords_71A

--- src/Problem_Solving/WayTooLongWords/Program.cs
+++ src/Problem_Solving/WayTooLongWords/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace WayTooLongWords_71A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputLineNumber = Convert.ToInt32(Console.ReadLine()); //how many line get user inpt
+            string[] str = new string[inputLineNumber];
+            string temp = string.Empty;
+            for (int i = 0; i < inputLineNumber; i++)
+            {
+                str[i] = Console.ReadLine() .ToLower();
+
+            }
+            for (int j = 0; j < str.Length; j++)
+            {
+                if (10 < str[j].Length)
+                {
+                    temp = str[j];
+
+                    int len = temp.Length - 2;
+
+                    Console.WriteLine(temp[0] + len.ToString() + temp[temp.Length - 1]);
+
+                }
+                else
+                {
+                    Console.WriteLine(str[j]);
+                }
+            }
+        }
+    }
+}
