72c1e3738c7ae1e08834e2754ac97b31a2c3d6db
Shamim448 <shamim.448@gmail.com>
2021-12-11 09:18:31 +0600

    update

---
+++ src/Problem_Solving/BoyOrGirl_236A/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BoyOrGirl_236A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine().ToLower();
+            string result = string.Empty;
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (!result.Contains(name[i]))
+                {
+                    result+=name[i];
+                }
+                
+            }
+            //Console.WriteLine(result);
+            if(result.Length % 2 == 0)
+            {
+                Console.WriteLine("CHAT WITH HER!");
+            }
+            else
+            {
+                Console.WriteLine("IGNORE HIM!");
+            }
+
+
+        }
+    }
+}
