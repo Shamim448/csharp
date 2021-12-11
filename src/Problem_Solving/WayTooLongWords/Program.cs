0764116d59df0b8ecbb31c7fc8f2a3d83c2956be
Shamim448 <shamim.448@gmail.com>
2021-12-11 14:49:07 +0600

    WayTooLongWords_71A comment update

--- src/Problem_Solving/WayTooLongWords/Program.cs
+++ src/Problem_Solving/WayTooLongWords/Program.cs
@@ -6,22 +6,26 @@ namespace WayTooLongWords_71A
     internal class Program
     {
         static void Main(string[] args)
-        {
-            int inputLineNumber = Convert.ToInt32(Console.ReadLine()); //how many line get user inpt
+        {//how many line get user input
+            int inputLineNumber = Convert.ToInt32(Console.ReadLine()); 
+            //Array store for user input 
             string[] str = new string[inputLineNumber];
             string temp = string.Empty;
+            //user input loop
             for (int i = 0; i < inputLineNumber; i++)
             {
                 str[i] = Console.ReadLine() .ToLower();
 
             }
+            //Output process loop
             for (int j = 0; j < str.Length; j++)
             {
+                //check lenght
                 if (10 < str[j].Length)
                 {
                     temp = str[j];
 
-                    int len = temp.Length - 2;
+                    int len = temp.Length - 2; //length integer
 
                     Console.WriteLine(temp[0] + len.ToString() + temp[temp.Length - 1]);
 
