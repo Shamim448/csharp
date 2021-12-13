9d2f5117117b57cfb3638a425ae8efd4623b26a9
Shamim448 <shamim.448@gmail.com>
2021-12-13 10:04:17 +0600

    Bit++_282A

---
+++ src/Problem_Solving/Bit++_282A/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Bit___282A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 0;
+            
+            int numberOfStatement = Convert.ToInt32(Console.ReadLine()); 
+            for (int i = 0; i < numberOfStatement; i++)
+            {
+                string result = Console.ReadLine();
+                if(result[0] == '-' || result[2] == '-')
+                {
+                    X -= 1;
+                }
+                else if(result[0] == '+' || result[2] == '+')
+                {
+                    X += 1;
+                }
+            }
+            Console.WriteLine(X);
+        }
+    }
+}
