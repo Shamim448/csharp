fc35901342cb5e062397ef886357c2b092a226e2
Shamim448 <shamim.448@gmail.com>
2021-12-14 10:51:16 +0600

    sort string using class method

---
+++ src/practice/SortString/sortString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    internal class sortString
+    {
+         internal static string SortString(string input)
+        {
+            char[] chars = input.ToCharArray();
+            Array.Sort(chars);
+            return new string (chars);
+        }
+    }
+}
