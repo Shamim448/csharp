6d51cd56b820be558c889f7a91b9ebc39796c45a
Shamim448 <shamim.448@gmail.com>
2021-12-19 10:19:36 +0600

    AbstractClass

---
+++ src/practice/AbstractClass/Discount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public abstract class Discount
+    {
+        public Discount(double amount)
+        {
+            Amount = amount;
+        }
+        public double Amount { get; set; }
+        public abstract double PriceAfterDiscount(double originalPrice);
+        //overload method
+        public abstract double PriceAfterDiscount(double originalPrice,  string s);
+    }
+}
---
+++ src/practice/AbstractClass/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AbstractClass;
+Discount discount = new DixedAmountDiscount();
+discount.Amount = 10;
+var fixedDiscount = discount.PriceAfterDiscount(1500);
+var percentageDiscount = discount.PriceAfterDiscount(1500, "per");
+Console.WriteLine(fixedDiscount);
+Console.WriteLine(percentageDiscount);
+
