27bed36013d0da0f5a6258bc9b20771b92845291
Shamim448 <you@example.com>
2021-12-22 11:55:32 +0600

    Assignment 1 done

--- src/Assignment1/Shape/triangle.cs
+++ src/Assignment1/Shape/triangle.cs
@@ -8,16 +8,16 @@ namespace Shapes
 {
     public class Triangle : Shape
     {
-        public double Width { get; set; }
+        public double Base { get; set; }
         public double Height { get; set; }
         public Triangle(double width, double height )
         {
-            this.Width = width;
+            this.Base = width;
             this.Height = height;
         }
         public override double Area()
         {
-            return 0.5 * Width * Height;
+            return 0.5 * Base * Height;
         }
         public override double Periameter()
         {
