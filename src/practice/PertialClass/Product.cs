872c2764e41774e82de8f26441483ab05b565199
Shamim448 <shamim.448@gmail.com>
2021-12-26 17:52:39 +0600

    Partial

---
+++ src/practice/PertialClass/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PertialClass
+{
+    internal partial class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double CalculatrDiscount(double discountPercentage)
+        {
+            return (Price * discountPercentage / 100);
+        }
+    }
+}
+ 
