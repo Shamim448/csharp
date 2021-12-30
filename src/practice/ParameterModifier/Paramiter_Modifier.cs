7b3e9454362e6114fcdf87bb65c2f5a58efb8006
Shamim448 <shamim.448@gmail.com>
2021-12-30 11:42:53 +0600

    parameter modifier add

---
+++ src/practice/ParameterModifier/Paramiter_Modifier.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterModifier
+{
+    internal class Paramiter_Modifier
+    {
+       //public int Sum(int num1, int num2)
+       // {
+       //     return num1 + num2;
+       // }
+       // public int Sum(int num1, int num2, int num3)
+       // {
+       //     return num1 + num2+ num3;
+       // }
+        public int Sum(params int[] array )
+        {
+            int result = 0;
+            foreach ( int item in array)
+            {
+                result = result + item;
+            }
+
+            return result;
+        }
+        public void UseParam(params object[] list)
+        {            
+            foreach (object item in list)
+            {
+                Console.Write("params parameter modifier =" + item + " ");
+            }
+            Console.WriteLine();// For blank line
+        }
+
+        //ref parameter modifier
+         public void Parametr_ref(ref int a) {
+            a = 10;
+        }
+        //in parameter modifier
+        public void Parametr_in(in int a)
+        {
+            //a = 10; we can't assign value but it's refarancrce type
+        }
+
+        //out parameter modifier
+        public void Parametr_out(out int a)
+        {
+            a = 20; //bust change valur and it value  type
+        }
+    }
+}
---
+++ src/practice/ParameterModifier/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ParameterModifier;
+
+Paramiter_Modifier paramiterModifier = new();
+var result = paramiterModifier.Sum(5, 3);
+var results = paramiterModifier.Sum(5, 3, 5);
+var resultArray = paramiterModifier.Sum(new int[] {2, 5, 6, 8});
+paramiterModifier.UseParam(new object[] { 1, 'a', "shamim", 5.5 });
+Console.WriteLine(result);
+Console.WriteLine(results);
+Console.WriteLine("params parameter modifier" + resultArray);
+
+//ref parameter modifier
+
+var x = 5;
+var y = 6;
+Console.WriteLine(x); //normally x = 5
+paramiterModifier.Parametr_ref(ref x);
+
+Console.WriteLine("Ref parameter modifier" + x); //after ref x = 10
+//because ref in referance type
+//in
+paramiterModifier.Parametr_in(in y);
+Console.WriteLine("In parameter modifier" + y);
+//out 
+paramiterModifier.Parametr_out(out y);
+Console.WriteLine("Out parameter modifier" + y);
+
