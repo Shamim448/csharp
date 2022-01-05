333d3bbfb1d05371d3a7ed4ffd3e360e2260212c
Shamim448 <shamim.448@gmail.com>
2022-01-05 18:56:12 +0600

    update

--- src/practice/Generic/GenericPoint.cs
+++ src/practice/Generic/GenericPoint.cs
@@ -6,9 +6,19 @@ using System.Threading.Tasks;
 
 namespace Generic
 {
-    internal class GenericPoint<T>
+    internal class GenericPoint<T> where T : struct
     {
         public T X { get; set; }
         public T Y { get; set; }
+
+        public GenericPoint<T> sum(T x, T y)
+        {
+            var newItem = new GenericPoint<T>();
+            //newItem.X += x;
+            newItem.X = x;
+            newItem.Y = y;
+            return newItem;
+        }
     }
 }
+ 
