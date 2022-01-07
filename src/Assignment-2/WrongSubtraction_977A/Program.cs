f668c99b2c014e4b03b1fbfc867d6addf6c12126
Shamim448 <you@example.com>
2022-01-07 11:01:52 +0600

    Assignment-2 Done

---
+++ src/Assignment-2/WrongSubtraction_977A/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WrongSubtraction_977A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] number = input.Split(" ");
+            int num1 = int.Parse(number[0]);
+            int num2 = int.Parse(number[1]);
+
+            int  r, i;
+            
+            for (i = 1; i <= num2; i++)
+            {
+                r = num1 % 10;
+                if (r == 0)
+                {
+                    num1 /= 10;
+                }
+                else
+                    num1 -= 1;
+            }
+            Console.WriteLine(num1);
+
+
+
+        }
+    }
+}
