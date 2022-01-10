7b24d362ccaa2dd4e63e840a57d255f171156391
Shamim448 <shamim.448@gmail.com>
2022-01-10 12:23:47 +0600

    ICollection

--- src/practice/ImportantInterface/People.cs
+++ src/practice/ImportantInterface/People.cs
@@ -7,10 +7,15 @@ using System.Threading.Tasks;
 
 namespace ImportantInterface
 {
-    internal class People : IEnumerable<Person>
+    internal class People : IEnumerable<Person>, ICollection<Person>
     {
         private Person[] _persons;
         private int index;
+
+        public int Count => index;
+
+        public bool IsReadOnly => true;
+
         public People(int size)
         {
             _persons = new Person[size];
@@ -30,6 +35,41 @@ namespace ImportantInterface
         public void Add(Person person)
         {
             _persons[index++] = person;  
+        }
+
+        public void Clear()
+        {
+
+            for (int i = 0; i < _persons.Length; i++)
+            {
+                _persons[i] = null;
+            }
+            index = 0;
+        }
+
+        public bool Contains(Person item)
+        {
+                for (int i = 0; i < index; i++)
+                {
+                    if(item.Name == _persons[i].Name)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+        public void CopyTo(Person[] array, int arrayIndex)
+        {
+            for (int i = 0; i < index; i++)
+            {
+                array[arrayIndex+i] = _persons[i];    
+            }
+        }
+
+        public bool Remove(Person item)
+        {
+            throw new NotImplementedException();
         }
     }
 }
