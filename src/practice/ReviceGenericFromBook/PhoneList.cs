f4a03275e7f68e354522c749247d9e7b403478e8
Shamim448 <you@example.com>
2022-01-12 12:05:39 +0600

    generic problem

---
+++ src/practice/ReviceGenericFromBook/PhoneList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviceGenericFromBook
+{
+    public class PhoneList<T> where T : struct
+    {
+       public T Num { get; set; }
+       
+,    }
+}
--- src/practice/ReviceGenericFromBook/Program.cs
+++ src/practice/ReviceGenericFromBook/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿using ReviceGenericFromBook;
+PhoneList<int> phoneList = new PhoneList<int>();
+phoneList.Num = 6;
+Console.WriteLine(phoneList.Num);
