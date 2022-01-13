57607ffb4a6e10e7a543c2751dee1c82a4abec41
Shamim448 <you@example.com>
2022-01-13 10:11:55 +0600

    Generic Method

---
+++ src/practice/GenericReviceForExam/GenericMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericReviceForExam
+{
+    internal class GenericMethods
+    {
+        public static bool CopyInsert<T>(T e, uint idx,T[] src, T[] target)
+        {
+            // See if target array is big enough.
+            if (target.Length < src.Length + 1)
+                return false;
+            // Copy src to target, inserting e at idx in the process.
+            for (int i = 0, j = 0; i < src.Length; i++, j++)
+            {
+                if (i == idx)
+                {
+                    target[j] = e;
+                    j++;
+                }
+                target[j] = src[i];
+            }
+            return true;
+        }
+    }
+}
--- src/practice/GenericReviceForExam/Program.cs
+++ src/practice/GenericReviceForExam/Program.cs
@@ -7,5 +7,40 @@ phonenumberlist.Add(new Person() { Name = "Saba", Number = 01746902499 });
 
 foreach(Person person in phonenumberlist._phoneNumbers)
 {
-    Console.WriteLine(person.Name);
+    //Console.WriteLine(person.Name);
 }
+
+
+
+//GenericMethods genmethod = new GenericMethods();
+
+int[] nums = { 1, 2, 3 };
+int[] nums2 = new int[4];
+// Display contents of nums.
+Console.Write("Contents of nums: ");
+foreach (int x in nums)
+    Console.Write(x + " ");
+Console.WriteLine();
+// Operate on an int array.
+GenericMethods.CopyInsert(99, 2, nums, nums2);
+// Display contents of nums2.
+Console.Write("Contents of nums2: ");
+foreach (int x in nums2)
+    Console.Write(x + " ");
+Console.WriteLine();
+
+
+// Now, use copyInsert on an array of strings.
+string[] strs = { "Generics", "are", "powerful." };
+string[] strs2 = new string[4];
+// Display contents of strs.
+Console.Write("Contents of strs: ");
+foreach (string s in strs)
+    Console.Write(s + " ");
+Console.WriteLine();
+// Insert into a string array.
+GenericMethods.CopyInsert("in C#", 1, strs, strs2);
+// Display contents of strs2.
+Console.Write("Contents of strs2: ");
+foreach (string s in strs2)
+    Console.Write(s + " ");
