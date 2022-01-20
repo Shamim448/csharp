560ea452741c2cc91f978c8e881cd282c887cf80
Shamim448 <you@example.com>
2022-01-20 08:28:44 +0600

    Adonet CRUD

---
+++ src/practice/AdonetCRUD/DataUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdonetCRUD
+{
+    internal class DataUtility
+    {
+       private readonly string _connectionString;
+
+        public DataUtility(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public void ExecuteCommend(string querycommend)
+        {
+           using SqlConnection connection = new SqlConnection(_connectionString);
+            using SqlCommand command = connection.CreateCommand();
+            command.CommandText = querycommend;
+            if(connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+            command.ExecuteNonQuery();
+                
+        }
+    }
+}
---
+++ src/practice/AdonetCRUD/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdonetCRUD;
+string connetionString = "Server=DESKTOP-7T438A0\\SQLEXPRESS;Database=CSharpB9;User Id=shamim;Password=saba2005;";
+//var query = "INSERT INTO Students ([Name], DateOfBirth) values ('Abdul Alim', '2010-08-28')";
+var query = "DELETE FROM Students where [Name] like '%shamim hosen%'";
+DataUtility datautility = new DataUtility(connetionString);
+datautility.ExecuteCommend(query);
