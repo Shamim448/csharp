86421e880872021892dc7d2a9389dfcb0a35c332
Shamim448 <shamim.448@gmail.com>
2022-01-23 18:47:11 +0600

    Entity Farmwork

--- src/practice/AdonetCRUD/DataUtility.cs
+++ src/practice/AdonetCRUD/DataUtility.cs
@@ -32,7 +32,7 @@ namespace AdonetCRUD
                 connection.Open();
             commend.ExecuteNonQuery();
         }
-<<<<<<< HEAD
+
         //Read data
         public IList<Dictionary<string, object>> DataReader(string query, IList<(string key, object value)> parameters)
         {
@@ -52,43 +52,6 @@ namespace AdonetCRUD
                 }
                 rows.Add(columns);
             }
-=======
-
-        //data read
-        public IList<Dictionary<string, object>> ReadData(string query,
-            IList<(string key, object value)> parameters)
-        {
-            using SqlConnection connection = new SqlConnection(_connectionString);
-            using SqlCommand commend = connection.CreateCommand();
-            commend.CommandText = query;
-            if (parameters != null && parameters.Count > 0)
-            {
-                foreach (var item in parameters)
-                {
-                    commend.Parameters.Add(new SqlParameter(item.key, item.value));
-                }
-            }
-            //check connection open or not
-            if (connection.State != System.Data.ConnectionState.Open)
-                connection.Open();
-
-            SqlDataReader reader = commend.ExecuteReader();
-
-            List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
-
-            while (reader.Read())
-            {
-                Dictionary<string, object> columns = new Dictionary<string, object>();
-
-                foreach (var column in reader.GetColumnSchema())
-                {
-                    columns.Add(column.ColumnName, reader[column.ColumnName]);
-                }
-
-                rows.Add(columns);
-            }
-
->>>>>>> 0a3f69dcc28d1db54f9f29e0cbbc859f50868da8
             return rows;
         }
     }
---
+++ src/practice/EntityFramwork/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramwork
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public decimal Fees { get; set; }
+        public DateTime RegistrationEnd { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
