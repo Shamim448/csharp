a378da50e2ca76cb4bb8f1ed55e295a115427fa4
Shamim448 <shamim.448@gmail.com>
2022-01-23 18:56:48 +0600

    Migration Generate

--- src/practice/EntityFramwork/CourseDbContext.cs
+++ src/practice/EntityFramwork/CourseDbContext.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EntityFramwork
 {
-    internal class CourseDBContent
+    public class CourseDbContext : DbContext
     {
+        private readonly string _connectionString;
+        private readonly string _assemblyName;
+
+        public CourseDbContext()
+        {
+            _connectionString = "";
+            _assemblyName = Assembly.GetExecutingAssembly().FullName;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            if (!dbContextOptionsBuilder.IsConfigured)
+            {
+                dbContextOptionsBuilder.UseSqlServer(_connectionString, m => m.MigrationsAssembly(_assemblyName));
+            }
+
+            base.OnConfiguring(dbContextOptionsBuilder);
+        }
+        public DbSet<Course> Courses { get; set; }
     }
 }
