4613011fa5aa38d1bd66c1bd9cc0e678d79e31b3
Shamim448 <you@example.com>
2022-01-30 19:30:06 +0600

    Events

---
+++ src/practice/Events/Botton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class Botton
+    {
+        public delegate void Onclick(string message);
+        public event Onclick click;
+
+        public void Activate(string message)
+        {
+            click.Invoke(message);
+        }
+    }
+}
---
+++ src/practice/Events/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Events;
+Botton botton = new Botton();
+botton.click += Printmessage;
+botton.click += Printmessage1;
+botton.Activate("Hello");
+void Printmessage(string messa) { 
+    Console.WriteLine(messa);
+}
+void Printmessage1(string messa)
+{
+    Console.WriteLine("Another message:"+messa);
+}
