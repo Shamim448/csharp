2bef2329f446f7a74017d8ddd082ad609c78a96e
Shamim448 <you@example.com>
2022-01-31 17:51:04 +0600

    Opening Screen Done

--- src/Assignment3/TrackRecordOfPharmacy/Program.cs
+++ src/Assignment3/TrackRecordOfPharmacy/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+MenuOption();
+//Opeaning Screen Menu
+static void MenuOption()
+{   
+    Console.Clear(); // Clear repiting Munu 
+    Console.WriteLine("Welcome To Our Apps");
+    Console.WriteLine(" ");
+    Console.WriteLine("a. Search Medicin");
+    Console.WriteLine("b. Add Medicin");
+    Console.WriteLine("c. Update Medicin");
+    Console.WriteLine("d. Remove Medicin");
+    Console.WriteLine("e. Exit Medicin");
+    Console.ReadLine();
+    //If User Press enter call Opening screen
+    MenuOption();
+}
