27e92c3b83261ce83d5fdfd870c10942a907a105
Shamim448 <shamim.448@gmail.com>
2022-02-02 17:00:08 +0600

    adonet done

--- src/practice/AdonetPractice/DataUtility.cs
+++ src/practice/AdonetPractice/DataUtility.cs
@@ -33,5 +33,36 @@ namespace AdonetPractice
                 connection.Open();
             command.ExecuteNonQuery();
         }
+
+        public IList<Dictionary<string, object>> 
+            ReadData(string query, IList<(string key, object value)> paramiter)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = query;
+            if (paramiter != null && paramiter.Count > 0)
+            {
+                foreach (var item in paramiter)
+                {
+                    command.Parameters.Add(new SqlParameter(item.key, item.value));
+                }
+
+            }
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+            List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+            while (reader.Read())
+            {
+                Dictionary<string, object> columns = new Dictionary<string, object>();
+                foreach(var item in reader.GetColumnSchema())
+                {
+                    columns.Add(item.ColumnName, reader[item.ColumnName]);
+                }
+                rows.Add(columns);
+            }
+            return rows;
+        }
     }
 }
