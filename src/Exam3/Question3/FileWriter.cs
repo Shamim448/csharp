892033f894b71b43fc12c358500a4a4259e4ccb1
Shamim448 <you@example.com>
2022-02-03 22:01:46 +0600

    up

--- src/Exam3/Question3/FileWriter.cs
+++ src/Exam3/Question3/FileWriter.cs
@@ -8,9 +8,41 @@ namespace Question3
 {
     internal class FileWriter
     {
-        public void WriteToFile(List<string> lines, string path)
+        //List<string> lines,
+        public void WriteToFile( string path)
         {
-            throw new NotImplementedException();
+           if(File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            using (FileStream file = File.Create(path))
+            {
+                file.Dispose();
+                using FileStream stream = File.Open(path, FileMode.Open);
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes("Hello World");
+
+                stream.Write(buffer);
+                stream.Flush();
+                FileInfo fileInfo = new FileInfo(path);
+                if (fileInfo.Exists)
+                {
+                    using (FileStream stream1 = fileInfo.Open(FileMode.Open))
+                    {
+                        long size = fileInfo.Length;
+                        for (long i = 0; i < size; i++)
+                        {
+                            byte[] bufferread = new byte[1];
+                            int read = stream.Read(buffer);
+                            string s = System.Text.Encoding.UTF8.GetString(buffer);
+                            Console.WriteLine(s);
+                        }
+                    }
+                    fileInfo.Delete();
+                }
+
+
+            }
+
         }
     }
 }
--- src/Exam3/Question3/Program.cs
+++ src/Exam3/Question3/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Question3;
+FileWriter fileWriter = new FileWriter();
+fileWriter.WriteToFile("../../../Hello.txt");
