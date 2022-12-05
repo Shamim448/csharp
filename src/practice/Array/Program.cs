e4859ee1f230fbf42b8f7a010992692572c1c5a9
Shamim448 <shamim.448@gmail.com>
2022-12-05 19:12:36 +0600

    multidimensional Array

--- src/practice/Array/Program.cs
+++ src/practice/Array/Program.cs
@@ -15,6 +15,9 @@ namespace Array // Note: actual namespace depends on the project name.
             int[] marks = new int[5] { 99, 100, 20, 85, 45};
             //Initialization with declaratinon omit size
             string[] names = new string[] { "Shamim", "Saba", "Fatema"};
+            //Copy array
+            int[] scores = marks;
+
 
             //Accessing the array
             Console.WriteLine(balance[0]);
@@ -30,6 +33,25 @@ namespace Array // Note: actual namespace depends on the project name.
                 int j = 0;
                 Console.WriteLine("Name[{0}] = {1}", j, i);
                 j++;
+            }
+
+            //-----------------Two Dimentional Array----------------
+            int[,] grid= new int[3,5]; //1st value row 2nd value column
+            grid[0,0] = 4;             // row 3 column 5
+            grid[0,1] = 8;
+            grid[1,3] = 6;
+            foreach(int i in grid)
+            {
+                Console.WriteLine(i);
+            }
+            //Assign value with declaretion
+            int[,] a = new int[3, 2] { { 5, 7 }, { 8, 6 }, { 9, 17 } }; // 3 row  2 cloumn
+            for(int i = 0; i < a.GetLength(0); i++) // loop for row 3
+            {
+                for(int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.WriteLine("Value[{0},{1}] : {2}", i, j, a[i, j]);
+                }
             }
 
         }
