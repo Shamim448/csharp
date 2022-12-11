65754f1ad9b8c6ce98b36324befad7905b61539d
Shamim448 <shamim.448@gmail.com>
2022-12-11 19:18:27 +0600

    Access Modifier

---
+++ src/practice/Access_Modifier/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Access_Modifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher Teachar = new Teacher();
+            Teachar.id = 1;
+            Teachar.name = "test";
+
+            Teacher teachar1 = new Teacher();
+            teachar1.id = 2;
+            teachar1.name = "saba";
+        }
+    }
+}
---
+++ src/practice/Access_Modifier/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifier
+{
+ class Teacher
+    {
+        public int id;
+        internal string name;
+    }
+}
