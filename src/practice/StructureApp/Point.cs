a13a333a1d5cb94f151dbf1e560565cbc52ac99f
Shamim448 <shamim.448@gmail.com>
2022-12-21 16:24:50 +0600

    StructureApp

---
+++ src/practice/StructureApp/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructureApp
+{
+    internal struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Distance( Point anotherpoints)
+        {
+            return (anotherpoints.X - X) + (anotherpoints.Y - Y);
+        }
+    }
+}
---
+++ src/practice/StructureApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using StructureApp;
+
+Point p1 = new Point();
+p1.X = 20; 
+p1.Y = 20;
+
+Point p2 = new Point();
+p2.X = 10;
+p2.Y = 20;
+
+var distance = p2.Distance(p1);
+Console.WriteLine(distance);
