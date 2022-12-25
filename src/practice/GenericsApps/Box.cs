db7975b24c8caa75883e1f00b976aa8b8f79ed96
Shamim448 <shamim.448@gmail.com>
2022-12-25 17:31:50 +0600

    Generics

---
+++ src/practice/GenericsApps/Box.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsApps
+{
+    internal class Box<T> where T : Item
+    {
+        T[] _items;
+        int _index;
+        public Box()
+        {
+            _index = 0;
+            _items = new T[_index++];
+
+        }
+        public Box(int size)
+        {
+            _index = 0;
+            _items = new T[size];
+
+        }
+        public void Add(T item)
+        {
+            if(_index < _items.Length)
+            {
+                _items[_index++] = item;
+            }
+        }
+        public T Get(int index)
+        {
+            if (index < _items.Length)
+            {
+                return _items[index];
+            }
+            else
+                return default(T);
+        }
+        public void Clear()
+        {
+            _index = 0;
+            for(int i = 0; i < _items.Length; i++)
+            {
+                _items[i] = null;
+            }
+        }
+    }
+}
---
+++ src/practice/GenericsApps/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using GenericsApps;
+
+Box <Book> b = new Box<Book>();
+b.Add(new Book() {Name ="Novel", Price = 320});
+b.Add(new Book() {Name ="C#", Price = 320});
+
+Box<Shart> s= new Box<Shart>();
+s.Add(new Shart() { Name = "Formal", Color = "Red" });
+s.Add(new Shart() { Name = "Casual", Color = "Black" });
+
+Book book = b.Get(1);
+Console.WriteLine(book.Name);
+
+
