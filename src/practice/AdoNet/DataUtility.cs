8e4fc9cfa5209a8b7d38ecf86f66ef21231195d1
Shamim448 <shamim.448@gmail.com>
2023-01-03 17:10:35 +0600

    Adonet insert and read

---
+++ src/practice/AdoNet/DataUtility.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNet
+{
+    internal class DataUtility
+    {
+        private readonly string _connectionString;
+        public DataUtility(string connectionString) 
+            { 
+                _connectionString = connectionString; 
+            }
+        public void ExecuteQuery(string querys, IList<(string key, object value)> parameters)
+        {
+            using SqlConnection connection = new(_connectionString);
+            using SqlCommand command = connection.CreateCommand();
+            command.CommandText = querys; //Write query here
+            if(parameters != null && parameters.Count > 0)
+            {
+                foreach(var item in parameters)
+                {
+                    command.Parameters.Add(new SqlParameter(item.key, item.value));
+                }
+            }
+            if(connection.State!= System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+        }
+        // Read Data From database
+        public List<Dictionary<string, Object>> ReadData(string query)
+        {
+            using SqlConnection connection = new(_connectionString);
+            using SqlCommand command = connection.CreateCommand();
+            command.CommandText = query;
+            if(connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                List<Dictionary<string, Object>> rows = new List<Dictionary<string, Object>>();
+                while(reader.Read())
+                {
+                    Dictionary<string, Object> columns = new Dictionary<string, Object>();
+                    foreach(var item in reader.GetColumnSchema()) 
+                    {
+                        columns.Add(item.ColumnName, reader[item.ColumnName]);
+                    }
+                    rows.Add(columns);
+                }
+            return rows;
+        }
+    }
+}
---
+++ src/practice/AdoNet/Program.cs
@@ -0,0 +1,33 @@
+﻿using AdoNet;
+using System.Reflection.PortableExecutable;
+
+string connectionString = "Server = IT-SHAMIM\\SQLEXPRESS; Database = CSharpB9; User Id = Shamim; Password = saba2005";
+string name = Console.ReadLine();
+string dateOfBirth = Console.ReadLine();
+List<(string key, object value)> parameter = new List<(string key, object value)>();
+parameter.Add(("name", name));
+parameter.Add(("dateOfBirth", dateOfBirth));
+string query = $"INSERT INTO Students (Name, DateOfBirth) values (@name, @dateOfBirth)";
+
+DataUtility dataUtility = new(connectionString);
+//dataUtility.ExecuteQuery(query, parameter);
+
+string read = "Select * from Students";
+var result = dataUtility.ReadData(read);
+if(result != null && result.Count > 0) { 
+    foreach(var header in result[0].Keys)
+    {
+        Console.Write(header);
+        Console.Write('\t');
+    }
+    Console.WriteLine();
+    foreach (var item in result)
+    {
+        foreach(var col in item.Values)
+        {
+            Console.Write(col);
+            Console.Write('\t');
+        }
+        Console.WriteLine();
+    }
+}
