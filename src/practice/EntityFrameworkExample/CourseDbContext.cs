e21dcb288f5f5967545e8b5b27d8cc7fd12d9c0e
Shamim448 <shamim.448@gmail.com>
2023-01-04 17:39:59 +0600

    Create entity farmwork

---
+++ src/practice/EntityFrameworkExample/CourseDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkExample
+{
+    internal class CourseDbContext : DbContext
+    {
+        private readonly string _connectionString;
+        private readonly string _assemblyName;
+
+        public CourseDbContext() {
+            _connectionString = "Server = IT-SHAMIM\\SQLEXPRESS; Database = CSharpB9; User Id = Shamim; Password = saba2005";
+            _assemblyName = Assembly.GetExecutingAssembly().FullName;
+        }
+
+        public CourseDbContext(string connectionString, string assemblyName)
+        {
+            _connectionString = connectionString;
+            _assemblyName = assemblyName;
+        }
+
+        //Create Connection
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            if (!dbContextOptionsBuilder.IsConfigured)
+            {
+                dbContextOptionsBuilder.UseSqlServer(_connectionString, m => m.MigrationsAssembly(_assemblyName));
+            }
+            base.OnConfiguring(dbContextOptionsBuilder);
+        }
+        public DbSet<Course> Courses { get; set; }
+
+    }
+}
