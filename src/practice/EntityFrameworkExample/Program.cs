8dc676504bff86e5a32681d8518da9aa03682053
Shamim448 <shamim.448@gmail.com>
2023-01-05 16:11:50 +0600

    Delete and show item

--- src/practice/EntityFrameworkExample/Program.cs
+++ src/practice/EntityFrameworkExample/Program.cs
@@ -1,11 +1,33 @@
 ﻿using EntityFrameworkExample;
 
 CourseDbContext courseDb = new();
-Course course = new Course();
-course.Name = "Asp dotNet";
-course.Fee = 30000;
-course.StartDate = DateTime.Now.AddDays(9);
-course.Description = "Full stack Asp .net MVC core";
+//Add value in table
+//Course course = new Course();
+//course.Name = "C#";
+//course.Fee = 9000;
+//course.StartDate = DateTime.Now.AddDays(20);
+//course.Description = "Full stack Asp .net MVC core";
 
-courseDb.Courses.Add(course);
-courseDb.SaveChanges();
+//Course course2 = new Course();
+//course2.Name = "Competative Beginner";
+//course2.Fee = 7000;
+//course2.StartDate = DateTime.Now.AddDays(19);
+//course2.Description = "Full stack Asp";
+
+//courseDb.Courses.Add(course);
+//courseDb.Courses.Add(course2);
+//courseDb.SaveChanges();
+
+
+//Delete specific row 
+//Course course = courseDb.Courses.Where(x => x.Id == 3).FirstOrDefault();
+//courseDb.Courses.Remove(course);
+//courseDb.SaveChanges();
+
+//Get all example
+
+List< Course > courses = courseDb.Courses.ToList();
+foreach( var item in courses)
+{
+    Console.WriteLine(item.Name);
+}
