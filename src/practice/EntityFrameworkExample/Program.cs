aad7320557c42bdadfbe6ec9fad83542422c3e53
Shamim448 <shamim.448@gmail.com>
2023-01-05 11:48:18 +0600

    Insert value

--- src/practice/EntityFrameworkExample/Program.cs
+++ src/practice/EntityFrameworkExample/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EntityFrameworkExample;
+
+CourseDbContext courseDb = new();
+Course course = new Course();
+course.Name = "Asp dotNet";
+course.Fee = 30000;
+course.StartDate = DateTime.Now.AddDays(9);
+course.Description = "Full stack Asp .net MVC core";
+
+courseDb.Courses.Add(course);
+courseDb.SaveChanges();
