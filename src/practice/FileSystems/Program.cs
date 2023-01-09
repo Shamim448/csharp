07c9f3afb6557158620e80846452a1529c49c410
Shamim448 <shamim.448@gmail.com>
2023-01-09 09:14:29 +0600

    Read File by fileinfo object

--- src/practice/FileSystems/Program.cs
+++ src/practice/FileSystems/Program.cs
@@ -1,11 +1,36 @@
-﻿string file = "../../../Hello.txt";
+﻿using System.Diagnostics.CodeAnalysis;
+
+string file = "../../../Hello.txt";
+//File.Create(file);
 if (File.Exists(file))
 {
     File.Delete(file);
     using FileStream myFile = File.Create(file);
 }
+//Write File
 using FileStream stream = File.Open(file, FileMode.Open);
-byte[] buffer = System.Text.Encoding.UTF8.GetBytes("Hello world");
-stream.Write(buffer);
-stream.Flush();
-stream.Close();
+    byte[] buffer = System.Text.Encoding.UTF8.GetBytes("Hello world");
+    stream.Write(buffer);
+    stream.Flush();
+    stream.Close();
+    stream.Dispose();
+
+
+
+//Read File using fileinfo object
+FileInfo fileInfo = new FileInfo(file);
+if (fileInfo.Exists)
+{
+    using (FileStream streams = fileInfo.Open(FileMode.Open))
+    {
+        long size = fileInfo.Length;
+        for(long i = 0; i < size; i++)
+        {
+            byte[] buffers = new byte[1];
+            streams.Read(buffers);
+            string s  = System.Text.Encoding.UTF8.GetString(buffers);
+            Console.Write(s);
+        }
+    }
+   // fileInfo.Delete();
+}
