8ff008d69e336ee3d82b3a96a9369c636b264652
Shamim448 <shamim.448@gmail.com>
2023-01-10 09:35:50 +0600

    Delegates

---
+++ src/practice/DelegatesExample/Delegates.cs
@@ -0,0 +1,13 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExample
+{
+    internal class Delegates
+    {
+        public delegate int Calculate(int x, int y);
+    }
+}
---
+++ src/practice/DelegatesExample/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using static DelegatesExample.Delegates;
+Calculate calculate = sum;
+int sum (int x, int y)
+{
+    return x + y;
+}
+
+void Print(Calculate calculate)
+{
+    var result = calculate(4, 5);
+    Console.WriteLine(result);
+}
+Print(calculate);
