b5012b110505a9c2c16b1c0f23d9ddf3916ddf73
Shamim448 <shamim.448@gmail.com>
2023-01-16 09:13:55 +0600

    Events

---
+++ src/practice/EventsExample/Button.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsExample
+{
+    public class Button
+    {
+        public delegate void ClickEvent(string text);
+        public event ClickEvent Click;
+
+       public void Activete(string message)
+        {
+            if(! string.IsNullOrWhiteSpace(message))
+            {
+                 Click.Invoke(message);
+            }
+            else
+            {
+                 Click.Invoke(string.Empty);
+            }
+        }
+    }
+}
---
+++ src/practice/EventsExample/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EventsExample;
+
+Button button = new Button();
+button.Click += PrintMessage;
+button.Click += PrintMessage2;
+button.Activete("Hello");
+void PrintMessage(string message)
+{
+    Console.WriteLine(message);
+}
+void PrintMessage2(string message)
+{
+    Console.WriteLine("Another " + message);
+}
