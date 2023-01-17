b55a1959c34860544b15333358b45079aa1e4600
Shamim448 <shamim.448@gmail.com>
2023-01-17 16:57:30 +0600

    Exception Handling

---
+++ src/practice/ExceptionHandelinng/DataUtility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandelinng
+{
+    internal class DataUtility
+    {
+        public async Task ReadFile(string path)
+        {
+            FileStream stream = null;
+            try
+            {
+                stream = File.Open(path, FileMode.Open);
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please put the acctual path");
+                path = Console.ReadLine();
+                stream = File.Open(path, FileMode.Open);
+            }
+            if(stream != null)
+            {
+                FileInfo fileInfo = new FileInfo(path);
+                for (int i = 0; i < fileInfo.Length; i++)
+                {
+                    byte[] buffer = new byte[1];
+                    stream.ReadAsync(buffer);
+                    Console.Write(System.Text.Encoding.UTF8.GetString(buffer));
+                }
+            }
+            
+        }
+    }
+}
---
+++ src/practice/ExceptionHandelinng/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace ExceptionHandelinng // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DataUtility dataUtility = new DataUtility();
+            dataUtility.ReadFile("../../../hello.txt");
+        }
+
+       
+    }
+
+
+}
