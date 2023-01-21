7c3677fe0a92000d5f5c8de80a8972193191e7a9
Shamim448 <shamim.448@gmail.com>
2023-01-21 09:08:46 +0600

    Linq

---
+++ src/practice/LinqExample/Program.cs
@@ -0,0 +1,7 @@
+﻿
+List<int> col = new List<int>() { 20, 29, 50, 38, 26, 64, 24, 88, 75 };
+var val = from i in col where i > 20 orderby i select i;
+foreach(var item in val)
+{
+    Console.WriteLine(item);
+}
